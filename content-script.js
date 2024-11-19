6737cf5f5795b268886917371fbb6b5b91b0515b
Nayem Majhar <nayemmajhar@gmail.com>
2024-11-19 18:33:12 +0100

    update - find the correct match for reels and suggested content

--- content-script.js
+++ content-script.js
@@ -1,8 +1,18 @@
 document.body.onload = () => {
     document.addEventListener("scroll", function (e) {
+        // Hide Reels and short videos suggestions
         for (let fbsugs of window.document.querySelectorAll('span[dir="auto"]')) {
             if(fbsugs.innerText === 'Suggested for you' || fbsugs.innerText === 'Reels and short videos'){
                 fbsugs.closest('div[aria-labelledby]').remove()
+            }
+        }
+
+        // Hide Suggested for you with Follow button
+        for (let fbfollow of window.document.querySelectorAll('div[role="button"]')) {
+            // child span has text 'Follow'
+            if(fbfollow.querySelector('span') && fbfollow.querySelector('span').innerText === 'Follow'){
+                fbfollow.closest('div.x1lliihq').remove()
+                
             }
         }
     })
